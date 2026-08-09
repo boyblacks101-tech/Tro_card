4054736bc095f990b3b17d6c037e947abc1c99aa
boyblacks101-tech <boyblacks101@gmail.com>
2026-08-09 17:21:42 +0330

    Update service worker cache version and files

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='tro-v6';
-const FILES=['./','index.html','core.js','data.js','manifest.json','icon.svg'];
+const C='tro-v8';
+const FILES=['./','index.html','core.js','core2.js','data.js','manifest.json','icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>clients.claim()))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(f=>{caches.open(C).then(c=>c.put(e.request,f.clone()));return f;})))});
