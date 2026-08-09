02dec5fc3cb1feaacf7931c16d839e98a95678ff
boyblacks101-tech <boyblacks101@gmail.com>
2026-08-09 13:54:32 +0330

    Upgrade service worker to version 4
    Updated service worker to version 4 and added activate event.

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-const C='tro-v2';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.json','icon.svg'])))});
+const C='tro-v4';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.json','icon.svg'])))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>clients.claim()))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(f=>{caches.open(C).then(c=>c.put(e.request,f.clone()));return f;})))});
