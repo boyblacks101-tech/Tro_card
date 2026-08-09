4602fabfecb24a292bea38b52e9b14d5b0c70834
boyblacks101-tech <boyblacks101@gmail.com>
2026-08-09 15:05:49 +0330

    Update service worker to version 5 and add files

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const C='tro-v4';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.json','icon.svg'])))});
+const C='tro-v5';
+const FILES=['./','index.html','style.css','core.js','data.js','manifest.json','icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>clients.claim()))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(f=>{caches.open(C).then(c=>c.put(e.request,f.clone()));return f;})))});
