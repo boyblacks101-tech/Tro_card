8c7fad6cfece7829a82cbd14305e4ad5db6a83d2
boyblacks101-tech <boyblacks101@gmail.com>
2026-08-10 07:39:42 +0330

    Rename REST system to Factory Reset with new functionality

--- rest.js
+++ rest.js
@@ -1,15 +1,14 @@
-/* REST SYSTEM */
+/* FACTORY RESET */
 (function(){
 if(window.__RS__)return;window.__RS__=1;
 var el=G.el,I=G.I;
-function R(){set.rest=set.rest||{last:0,buff:0};return set.rest;}
-var _mu=G.mult;G.mult=function(){return _mu()+((R().buff||0)>Date.now()?0.1:0);};
-window.doRest=function(){var r=R(),now=Date.now();
-if(now-r.last<6*3600000){var h=Math.ceil((6*3600000-(now-r.last))/3600000);return sysMsg('NOT TIRED YET',I('clock')+' You can rest again in <b>'+h+'h</b>.');}
-r.last=now;r.buff=now+3600000;G.mp();set.mp.cur=set.mp.max;save();
-sysMsg('RESTED',I('moon')+' Even hunters need rest.<br><b>MP restored • +10% XP for 1 hour</b>');};
-function rbtn(){if($('restBtn'))return;var r=document.querySelector('#scrHome .row');if(r)r.insertAdjacentHTML('beforebegin','<button id="restBtn" class="btn big" onclick="doRest()">🛌 REST</button>');}
-setInterval(function(){rbtn();if($('restBtn'))$('restBtn').style.borderColor=((R().buff||0)>Date.now()?'var(--acc)':'');},2000);
-setTimeout(rbtn,1200);
+var armed=false;
+window.factoryReset=function(){
+if(!armed){armed=true;var b=$('rsBtn');if(b)b.innerHTML='⚠️ SURE? TAP AGAIN';
+sysMsg('WARNING',I('warn')+' <b>FACTORY RESET</b> erases EVERYTHING:<br>cards, XP, levels, shadows, bonds, titles, stats...<br>You will restart as the Weakest Hunter.<br><br>Tap the button again within 5s to confirm.');
+setTimeout(function(){armed=false;var b2=$('rsBtn');if(b2)b2.innerHTML='♻️ RESET';},5000);return;}
+localStorage.clear();location.reload();};
+function rbtn(){if($('rsBtn'))return;var r=document.querySelector('#scrHome .row');if(r)r.insertAdjacentHTML('beforebegin','<button id="rsBtn" class="btn big" style="color:#ff453a;border-color:#662b2b" onclick="factoryReset()">♻️ RESET</button>');}
+setInterval(rbtn,2000);setTimeout(rbtn,1200);
 console.log('rest ok');
 })();
